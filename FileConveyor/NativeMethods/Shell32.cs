6d9b3868f8530a97ef9b80b454c6dc8a22745c59
Yuki Nishina <project_flower@outlook.com>
2024-01-29 23:19:19 +0900

    基本機能を実装

---
+++ FileConveyor/NativeMethods/Shell32.cs
@@ -0,0 +1,14 @@
+﻿namespace NativeMethods
+{
+    public static partial class Shell32
+    {
+        #region Public Fields
+
+        /// <summary>
+        /// shell32.dll のファイル名
+        /// </summary>
+        public const string LibraryName = "shell32.dll";
+
+        #endregion
+    }
+}
--- FileConveyor/Properties/AssemblyInfo.cs
+++ FileConveyor/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FileConveyor")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright © Project Flower 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
